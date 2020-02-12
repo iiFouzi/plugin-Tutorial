12d0aac47d48a271ecce98887cdd9918b7246b39
iiFouzi <60854634+iiFouzi@users.noreply.github.com>
2020-02-12 20:06:41 +0100

    Create Main.php

---
+++ src/iiFouzi/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace iiFouzi; // here you should put your plugin name or what  you wrote in plugin.yml .
+
+use pocketmine\Server;
+use pocketmime\Player;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\Command 
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase {
+    
+    public function onEnable() {
+    }
+    
+    public function onCommand(CommandSender $player, Command $cmd, string $label, array $args) :bool {
+        
+        switch($cmd->getName()){
+        case "tutorial": // The command
+        if($player instanceof Player){
+            $player->sendMessage("welcome to your server , have a good day") // when a player use the commad it will send him this message
+            }
+            break;
+            }
+            
+            return true;
+            
+     }
+            
+            
+}
